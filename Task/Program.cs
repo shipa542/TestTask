fb447e631e74b890ad8fe7ffdd752f24440cf1d2
Shipa542 <shipa542@yandex.ru>
2022-08-08 23:09:39 +0300

    Добавлен алгоритм

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,7 +5,7 @@ void SecondArrayWithIF(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length == 3)
+    if(array1[i].Length <= 3)
         {
         array2[count] = array1[i];
         count++;
@@ -16,7 +16,7 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}");
     }
     Console.WriteLine();
 }
